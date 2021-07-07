0717602c15d499fca077190121af69f65745842b
marcin <marcin.klementowski@gmail.com>
2021-07-07 19:49:39 +0200

    Add TaskController and TaskService (issue) v2

--- src/main/java/com/taskmanager/tm/services/task/TaskService.java
+++ src/main/java/com/taskmanager/tm/services/task/TaskService.java
@@ -1,12 +1,17 @@
 package com.taskmanager.tm.services.task;
 
+import com.taskmanager.tm.entities.project.Project;
+import com.taskmanager.tm.entities.sprint.Sprint;
 import com.taskmanager.tm.entities.task.Task;
+import com.taskmanager.tm.entities.user.User;
 import com.taskmanager.tm.enums.Priority;
 import com.taskmanager.tm.enums.TaskStatus;
 import com.taskmanager.tm.enums.TaskType;
 import com.taskmanager.tm.repositories.task.TaskRepository;
+import com.taskmanager.tm.repositories.user.UserRepository;
 import com.taskmanager.tm.services.dto.task.TaskDTO;
 import com.taskmanager.tm.services.exceptions.RequestException;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +23,13 @@ import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class TaskService {
 
     private final Logger log = LoggerFactory.getLogger(TaskService.class);
 
-    @Autowired
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
 
     public Task createTask(TaskDTO taskDTO) {
         Task task = new Task();
@@ -58,14 +64,25 @@ public class TaskService {
         } else {
             task.setTaskStatus(TaskStatus.NEW);
         }
-        task.setSprint(taskDTO.getSprint());
+        Sprint sprint = new Sprint();
+        sprint.setName(taskDTO.getSprint().getName());
+        task.setSprint(sprint);
+        Project project = new Project();
+        project.setName(taskDTO.getSprint().getName());
+        task.setProject(project);
         task.setProject(taskDTO.getProject());
         if (taskDTO.getReportedUser() != null) {
-            task.setReportedUser(taskDTO.getReportedUser());
+            User user = userRepository.save(new User(taskDTO.getReportedUser().getName(), taskDTO.getReportedUser().getSurname(), taskDTO.getReportedUser().isActive()));
+            task.setReportedUser(user);
         } else {
             throw new RequestException("The reportedUser of the new task cannot be empty", "Task");
         }
-        task.setAssignedUser(taskDTO.getAssignedUser());
+        if (taskDTO.getAssignedUser() == null) {
+            task.setAssignedUser(null);
+        } else {
+            User user = userRepository.save(new User(taskDTO.getAssignedUser().getName(), taskDTO.getAssignedUser().getSurname(), taskDTO.getAssignedUser().isActive()));
+            task.setAssignedUser(user);
+        }
         task.setAttachments(taskDTO.getAttachments());
         this.taskRepository.save(task);
         log.debug("Created task: {}", task);
