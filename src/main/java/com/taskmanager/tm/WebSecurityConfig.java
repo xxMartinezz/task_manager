46033a7a6a6716177cc877949abe022b91f07e1d
marcin <marcin.klementowski@gmail.com>
2023-01-22 20:22:15 +0100

    Enabling CORS

---
+++ src/main/java/com/taskmanager/tm/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.taskmanager.tm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
